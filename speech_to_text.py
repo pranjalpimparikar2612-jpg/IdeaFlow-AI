3ffc0f3dc14fdd35bf217884dfb6ef4a186476b6
Pranjal Pimparikar <pranjal.pimparikar2612@gmail.com>
2026-08-17 20:39:58 +0530

    speech

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,3 +1,23 @@
+# import os
+# import whisper
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# ffmpeg_path = os.getenv("FFMPEG_PATH")
+
+# if ffmpeg_path:
+#     os.environ["PATH"] += os.pathsep + ffmpeg_path
+
+# # Load Whisper Tiny model
+# model = whisper.load_model("tiny")
+
+
+# def transcribe_audio(file_path):
+#     result = model.transcribe(file_path)
+#     return result["text"]
+
+
 import os
 import whisper
 from dotenv import load_dotenv
@@ -9,10 +29,15 @@ ffmpeg_path = os.getenv("FFMPEG_PATH")
 if ffmpeg_path:
     os.environ["PATH"] += os.pathsep + ffmpeg_path
 
-# Load Whisper Tiny model
-model = whisper.load_model("tiny")
+model = None
 
 
 def transcribe_audio(file_path):
+    global model
+
+    if model is None:
+        model = whisper.load_model("tiny")
+
     result = model.transcribe(file_path)
+
     return result["text"]
